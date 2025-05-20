b73c37dd80f15277bd43bd087350d9a908a812c4
Olin Kirkland <olin.kirkland@beg.de>
2025-05-20 08:08:22 +0200

    Refactor inputgroup

--- src/components/ui/InputGroup.vue
+++ src/components/ui/InputGroup.vue
@@ -3,27 +3,57 @@
         <div class="prepend" :class="{ 'is-slot-empty': isSlotEmpty }">
             <slot></slot>
         </div>
-        <input v-bind="attrs" :value="modelValue" @input="onInput" />
+        <input
+            v-bind="attrs"
+            :value="modelValue"
+            @input="onInput"
+            @keydown.enter="onPressEnterKey"
+            ref="input"
+            autocomplete="off"
+            @focus="showClearButton = true"
+            @blur="onBlur"
+        />
+        <button
+            class="clear"
+            @mousedown="onClickClear"
+            :class="{ show: showClearButton && modelValue.length > 0 }"
+            tabindex="-1"
+        >
+            <i class="fas fa-times-circle"></i>
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed, defineEmits, defineProps, useAttrs, useSlots } from 'vue';
+import { computed, defineEmits, defineProps, ref, useAttrs, useSlots } from 'vue';
 
 const props = defineProps<{
     modelValue: string;
 }>();
 
+const input = ref<HTMLInputElement | null>(null);
+
+const showClearButton = ref(false);
+
 const emit = defineEmits<{
     (e: 'update:modelValue', value: string): void;
 }>();
 
-const attrs = useAttrs();
-const slots = useSlots();
+const attrs: Record<string, unknown> = useAttrs();
+const slots: Record<string, unknown> = useSlots();
+
+function onBlur() {
+    // Wait for the next frame to hide the clear button
+    // This is a workaround for the issue where the button is hidden immediately after clicking it (iOS)
+    requestAnimationFrame(() => {
+        showClearButton.value = false;
+    });
+}
 
 const isSlotEmpty = computed(() => {
+    //@ts-ignore
     const defaultSlot = slots.default?.();
-    const children = defaultSlot?.filter((v) => v.type !== Comment && v.type !== Text) ?? [];
+    const children = defaultSlot?.filter((v: any) => v.type !== Comment && v.type !== Text) ?? [];
     if (children.length === 0) return true;
     const firstChild = children[0];
     return firstChild?.children?.length === 0;
@@ -33,23 +63,36 @@ function onInput(event: Event) {
     const target = event.target as HTMLInputElement;
     emit('update:modelValue', target.value);
 }
+
+function onClickClear() {
+    // Focus the input before clearing because on iOS the input will lose focus
+    // and the cursor will be removed when the input is cleared
+    emit('update:modelValue', '');
+    requestAnimationFrame(() => {
+        if (input.value) input.value.focus();
+    });
+}
+
+function onPressEnterKey() {
+    if (input.value) input.value.blur();
+}
 </script>
 
 <style scoped lang="scss">
 .input-group {
+    position: relative;
     display: flex;
     align-items: center;
     gap: 0.4rem;
     border-radius: 5px;
-    overflow: hidden;
     height: 3.2rem;
     width: 100%;
     min-height: 3.2rem;
     background-color: var(--color-background);
     border: 1px solid var(--color-surface-alt);
     padding-right: 0.8rem;
-
     outline: none;
+    overflow: hidden;
 
     &:focus-within {
         outline: 2px solid var(--color-primary-alt);
@@ -58,13 +101,15 @@ function onInput(event: Event) {
 
     > .prepend {
         display: flex;
-        gap: 0.8rem;
+        flex-shrink: 0;
+        gap: 1rem;
         align-items: center;
         padding: 0 1.2rem;
         background-color: var(--color-surface);
         margin-right: 0.6rem;
         height: 100%;
-        border-right: 1px solid var(--color-surface-alt);
+        border-right: 1px solid var(--color-urface-alt);
+        white-space: nowrap;
     }
 
     .is-slot-empty {
@@ -74,8 +119,13 @@ function onInput(event: Event) {
     }
 
     > input {
+        display: block;
+        width: 100%;
+        overflow: hidden;
         height: 100%;
         background-color: transparent;
+        color: var(--color-primary);
+
         &:focus {
             outline: none;
         }
@@ -83,7 +133,22 @@ function onInput(event: Event) {
             opacity: 0.5;
             font-style: italic;
         }
-        flex: 1;
+    }
+
+    > button.clear {
+        background: transparent;
+        border: none;
+        color: var(--color-primary);
+        cursor: pointer;
+        font-size: 1.2rem;
+        width: 2rem;
+        height: 100%;
+        left: 0.4rem;
+        display: none;
+
+        &.show {
+            display: unset;
+        }
     }
 }
 </style>
